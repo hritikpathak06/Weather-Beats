87fdf1a521ee794fba39ac16ec144ba2b6e1b6c8
Ritik Kumar Pathak <phritik06@gmail.com>
2024-09-24 13:30:39 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 const apiKey = "4dc92fa3a5bbb7a7c7c527eb067f7d45";
-const city = "Bhopal"; 
+const city = "Bhopal";
 const weatherInfoElement = document.getElementById("weather-info");
 const sendSongButton = document.getElementById("send-song");
-
 async function fetchWeather() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   try {
@@ -15,20 +13,14 @@ async function fetchWeather() {
     weatherInfoElement.textContent = "Error fetching weather data";
   }
 }
-
 function displayWeather(data, weatherCondition) {
   weatherInfoElement.textContent = `Current Weather in ${data.name}: ${data.main.temp}°C, ${weatherCondition}`;
-
-
   const songRecommendation = getSongRecommendation(weatherCondition);
-
-
   sendSongButton.style.display = "block";
   sendSongButton.addEventListener("click", () =>
     sendSongOnWhatsApp(songRecommendation)
   );
 }
-
 function getSongRecommendation(weatherCondition) {
   let songUrl;
   switch (weatherCondition) {
@@ -49,13 +41,11 @@ function getSongRecommendation(weatherCondition) {
   }
   return songUrl;
 }
-
 function sendSongOnWhatsApp(songUrl) {
   const phoneNumber = prompt(
     "Enter your phone number (with country code)",
     "+91"
   );
-
   if (phoneNumber) {
     const message = `Check out this song for the weather! \nClick here to listen: ${songUrl}`;
 
